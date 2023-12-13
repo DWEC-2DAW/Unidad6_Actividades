e12eba199f3221dadae6778f51e3fd096fea744d
davidlaekur <davidlaekur@me.com>
2023-12-13 18:16:39 +0100

    feat: ejercicio1

--- ej1_unidad6.js
+++ ej1_unidad6.js
@@ -3,3 +3,22 @@ Utilizando la función asyncRequest() realiza de forma secuencial peticiones par
 recurso1, recurso2 y recurso3, en ese orden. Cuando hayas obtenido los tres recursos debes
 imprimir “¡Completado!” en la consola */
 
+
+import { asyncRequest,/*  testFunction */ } from './asyncRequest.js';
+/* 
+testFunction(); */
+
+function peticionesSecuenciales() {
+    asyncRequest("resource1", function(result1) {
+        console.log(result1);
+        asyncRequest("resource2", function(result2) {
+            console.log(result2);
+            asyncRequest("resource3", function(result3) {
+                console.log(result3);
+                console.log("¡Completado!");
+            });
+        });
+    });
+}
+
+peticionesSecuenciales();
